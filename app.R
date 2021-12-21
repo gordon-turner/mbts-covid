4d2e30ac62bcd5683d4a11f9792dec8e57ed52a6
Gordon Turner <gordon.m.turner@gmail.com>
2021-12-20 22:33:05 -0500

    added schools data and revamped rolling average line for graph

--- app.R
+++ app.R
@@ -19,9 +19,23 @@ url <- "https://docs.google.com/spreadsheets/d/1poSbKDSkWFOewSkB-7mswwxJm20EVfTU
 
 gs4_deauth()  #don't need authorization for a public shared link
 
-covid <- read_sheet(url)%>% 
+covid <- read_sheet(url, sheet = "town")%>% 
     arrange(Date) %>% 
     mutate(new_cases = total_cases - lag(total_cases))
+
+school <- read_sheet(url, sheet = "school")%>% 
+    arrange(Date) %>% 
+    mutate(total_cases = cumsum(new_cases))
+    #mutate(new_cases = total_cases - lag(total_cases))
+
+school <- school %>% 
+    group_by(Date) %>% 
+    summarise(new_cases = sum(new_cases)) %>% 
+    arrange(Date) %>% 
+    mutate(total_cases = cumsum(new_cases))
+
+#school_totals <- school %>% 
+#    pivot_wider( names_from = school, values_from = new_cases, values_fn = cumsum)
 
 # covid <- read_excel(here("data", "mbts_covid.xlsx")) %>% 
 #     arrange(Date) %>% 
@@ -38,15 +52,31 @@ ui <- fluidPage(
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            varSelectInput("covid_col",
-                        "Data:",
-                        covid %>% select(-Date))
+            selectInput("source", 
+                        label = "Source", 
+                        choices=c("Town", "Schools"), 
+                        selected = "Town"),
+            conditionalPanel("input.source=='Town'",
+                             varSelectInput("data_col",
+                                            "Data:",
+                                            covid %>% select(-Date), 
+                                            selected = "new_cases")
+                             
+                             ),
+            conditionalPanel("input.source=='Schools'",
+                             varSelectInput("school_data_col",
+                                            "Data:",
+                                            school %>% select(new_cases, total_cases), 
+                                            selected = "new_cases")
+                             
+            )
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
            plotlyOutput("covidPlot"),
-           helpText("Data is collected from weekly", a(href="http://manchester.ma.us/337/Board-of-Health", "Manchester-By-The-Sea Board of Health updates")),
+           helpText("Town data is collected from weekly", a(href="http://manchester.ma.us/337/Board-of-Health", "Manchester-By-The-Sea Board of Health updates")),
+           helpText("School data is collected from superintendent emails and cross-checked with", a(href="https://ma01807435.schoolwires.net/Page/1557", "district monthly updates")),
            helpText("Tabulated data is available for download from", a("google sheets", href=url)),
            helpText("Source code is available for download from", a(href="https://github.com/gordon-turner/mbts-covid", "github"))
            
@@ -60,12 +90,32 @@ server <- function(input, output) {
 
     output$covidPlot <- renderPlotly({
 
-        gg <- ggplot(covid, aes_string(x="Date", y=input$covid_col)) + 
-            geom_col()+
-            geom_smooth(method = "loess", span=0.5)
-#            geom_line(aes(x=Date, y=rollmean(!!!input$covid_col, 3, na.pad=TRUE)))
+        if (input$source=="Town"){
+            gg <- ggplot(covid, aes_string(x="Date", y=input$data_col)) + 
+                geom_col()+
+                geom_line(aes(x=Date, 
+                              y=rollmean(!!!input$data_col, 4, na.pad=TRUE, align="right"),
+                              color = "4 week rolling avg."
+                ) 
+                )+
+                scale_color_manual(name = NULL, values = c("4 week rolling avg." = "black"))            
+        }else if (input$source=="Schools"){
+            gg <- ggplot(school, aes_string(x="Date", y=input$school_data_col)) + 
+                geom_col(position=position_dodge())
+                # geom_line(aes(x=Date, 
+                #               y=rollmean(!!!input$school_data_col, 4, na.pad=TRUE, align="right"),
+                #               color = "rolling avg."
+                # ) 
+                # )+
+                # scale_color_manual(name = NULL, values = c("rolling avg." = "black"))
+        }
+
             
-        ggplotly(gg)
+        ggplotly(gg)%>%
+            layout(legend = list(
+                orientation = "h"
+            )
+            )
     })
 }
 
---
+++ dev/graphs.R
@@ -0,0 +1,23 @@
+
+
+
+ggplot(covid, aes_string(x="Date", y="total_cases")) + 
+  geom_col()+
+  geom_line(aes(x=Date, 
+                y=rollmean(total_cases, 4, na.pad=TRUE, align="right"),
+                color = "4 week rolling avg."
+  ) 
+  )+
+  scale_color_manual(name = NULL, values = c("4 week rolling avg." = "black"))
+
+
+
+
+ggplot(school, aes_string(x="Date", y="total_cases")) + 
+  geom_col(aes(fill=school))+
+  geom_line(aes(x=Date, 
+                y=rollmean(total_cases, 4, na.pad=TRUE, align="right"),
+                color = "rolling avg."
+  ) 
+  )+
+  scale_color_manual(name = NULL, values = c("rolling avg." = "black"))
